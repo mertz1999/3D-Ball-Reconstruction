a1858aa86a48877b697b01d1cc676618aaf987e6
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-07-29 11:52:46 +0430

    (FEAT) Usee this file to making array like of base cordinates in 3D format and 2D image

---
+++ make_data.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+# File name 
+name = 'game_8'
+
+# Data in this format: X,Y,Z x,y
+data = [
+    [0,-6,0,100,581],
+    [0,-12,0,311,494],
+    [9,-12,0,964,485],
+    [9,-6,0,1180,562],
+    [0,-9,2.24,223,320],
+    [9,-9,2.24,1044,308],
+]
+
+
+data = np.array(data)
+
+np.save('./data/'+name+'.npy', data)
