f3a3c7e61d1cebdbf1091f5bb864a4f88e37562c
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-08-01 16:08:10 +0430

    (ADD) run and add with new data

--- make_data.py
+++ make_data.py
@@ -1,20 +1,123 @@
 from turtle import clear
 import numpy as np 
 
-# File name 
-name = 'game_8'
+# # File name 
+# name = 'game_8'
 
-# Data in this format: X,Y,Z x,y
+# # Data in this format: X,Y,Z x,y
+# data = [
+#     [0,-6,0,100,581],
+#     [0,-12,0,311,494],
+#     [9,-12,0,964,485],
+#     [9,-6,0,1180,562],
+#     [0,-9,2.24,223,320],
+#     [9,-9,2.24,1044,308],
+# ]
+
+
+# name = 'game_8_zoomed'
+
+# data = [
+#     [0,-6,0,53,362],
+#     [0,-12,0,267,276],
+#     [9,-12,0,921,267],
+#     [9,-6,0,1140,345],
+#     [0,-9,2.24,177,103],
+#     [9,-9,2.24,1001,90],
+# ]
+
+
+
+# name = '1_p1'
+# scale = 2.66
+# data = [
+#     [0,-9,2.24,188/scale, 301/scale],
+#     [9,-9,2.24,757/scale,320/scale],
+#     [0,-6,0   ,242/scale,520/scale],
+#     [9,-6,0   ,875/scale,481/scale],
+#     [0,0,0    ,496/scale,635/scale],
+#     [9,0,0    ,1267/scale,544/scale]
+# ]
+# data = np.array(data)
+# np.save('./data/'+name+'.npy', data)
+
+
+# name = '2_p1'
+# scale = 2.66
+# data = [
+#     [0,-9,2.24     ,411/scale,247/scale],
+#     [9,-9,2.24     ,1072/scale,233/scale],
+#     [0,-9,2.24+0.8 ,408/scale,190/scale],
+#     [9,-9,2.24+0.8 ,1072/scale,162/scale],
+#     [4.5,0,0       ,216/scale,496/scale],
+#     [9,0,0         ,823/scale,534/scale]
+# ]
+# data = np.array(data)
+# np.save('./data/'+name+'.npy', data)
+
+name = '3_p1'
+scale = 2.66
 data = [
-    [0,-6,0,100,581],
-    [0,-12,0,311,494],
-    [9,-12,0,964,485],
-    [9,-6,0,1180,562],
-    [0,-9,2.24,223,320],
-    [9,-9,2.24,1044,308],
+    [0,-9,2.24     ,1214/scale,144/scale],
+    [9,-9,2.24     ,627/scale,177/scale],
+    [0,-9,2.24+0.8 ,1219/scale,77/scale],
+    [9,-9,2.24+0.8 ,626/scale,133/scale],
+    [0,-18,0       ,931/scale,456/scale],
+    [9,-18,0       ,119/scale,376/scale]
 ]
-
-
 data = np.array(data)
-
 np.save('./data/'+name+'.npy', data)
+
+
+# name = '4_p1'
+# scale = 2.66
+# data = [
+#     [0,-9,2.24     ,739/scale,125/scale],
+#     [9,-9,2.24     ,246/scale,156/scale],
+#     [9,-12,0       ,375/scale,390/scale],
+#     [9,-6,0        ,184/scale,294/scale],
+#     [0,-12,0       ,865/scale,284/scale],
+#     [0,-18,0       ,1230/scale, 373/scale]
+# ]
+# data = np.array(data)
+# np.save('./data/'+name+'.npy', data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = 'volley_cords'
+# scale = 1
+# data = [
+#     [0,0,0],    
+#     [0,-18,0],
+#     [9,-18,0],
+#     [9,0,0],
+#     [0,-6,0],
+#     [0,-9,0],
+#     [0,-12,0],
+#     [9,-12,0],
+#     [9,-9,0],
+#     [9,-6,0],
+#     [0,-9,0.75],
+#     [0,-9,2.24],
+#     [9,-9,2.24],
+#     [9,-9,0.75]
+# ]
+
+# data = np.array(data)
+# np.save('./data/'+name+'.npy', data)
